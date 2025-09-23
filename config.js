fc8a2786422c816cab434891d4a7bef2297b3a89
BryanBel <bryanbelandriav@gmail.com>
2025-09-23 14:49:52 -0400

    new page

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-    ? 'placeholder'
+    ? 'https://to-do-1ti4.onrender.com/'
     : 'http://localhost:3000';
 
 const MONGO_URI = process.env.NODE_ENV === 'production'
